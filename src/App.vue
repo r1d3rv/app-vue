19f93a25681a8ad3a17bd40f605dc272a260d44c
r1d3rv <ridervargas-sif@itpm.edu.bo>
2026-06-02 19:35:57 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+// Contrato y datos
+interface Producto {
+  id: number;
+  nombre: string;
+  precio: number;
+  stock: number;
+}
+
+const listaProductos: Producto[] = [
+  {
+    id: 101,
+    nombre: "Teclado Mecánico RGB",
+    precio: 250,
+    stock: 12
+  },
+  {
+    id: 102,
+    nombre: "Mouse Óptico Inalámbrico",
+    precio: 110,
+    stock: 20
+  }
+];
+</script>
+
+<template>
+  <div class="container mt-5 text-center" style="font-family: sans-serif;">
+    <div class="alert alert-success shadow">
+      <h1>Panel de Almacén - ITPM</h1>
+      <p>Asignatura: DPW-207 | Lic. Félix Maldonado</p>
+      <hr>
+
+      <ul style="list-style: none; padding: 0;">
+        <li
+          v-for="p in listaProductos"
+          :key="p.id"
+          style="margin-bottom: 10px;"
+        >
+          <b>{{ p.nombre }}</b>
+          - Precio: Bs. {{ p.precio }}
+          | Stock: {{ p.stock }} pzas.
+        </li>
+      </ul>
+    </div>
+    <p>Vargas Meneces Rider Guido</p>
+  </div>
+</template>
